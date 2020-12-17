966801e61718802f693db546bfe0447859c7253e
Ibrahim aljabr <Ibrahim_i95@hotmail.com>
2020-12-17 04:27:17 +0200

    first js

---
+++ first.js
@@ -0,0 +1,17 @@
+let name = prompt("What is your name?")
+alert("Hello " + name)
+
+
+function myFunction() {
+let fcolor = prompt("What is your favorite color?")
+
+      document.body.style.backgroundColor = fcolor;
+
+      if(fcolor=="black")
+       { 
+          alert("It's my favorite color to :)")
+       }
+
+  }  
+            
+  myFunction();
